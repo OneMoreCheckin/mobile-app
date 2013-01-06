572fb74734f22a19ea4b5e9a18a29106f913438c
Manu <e.tabard@gmail.com>
2013-01-06 01:00:21 +0100

    ignore

---
+++ projects/ios/OneMoreCheckIn/Plugins/TapToTop.h
@@ -0,0 +1,13 @@
+//
+//  TapToTop.h
+//  OneMoreCheckIn
+//
+//  Created by Manu on 05/01/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TapToTop : NSObject
+
+@end
---
+++ projects/ios/OneMoreCheckIn/Plugins/TapToTop.m
@@ -0,0 +1,13 @@
+//
+//  TapToTop.m
+//  OneMoreCheckIn
+//
+//  Created by Manu on 05/01/13.
+//
+//
+
+#import "TapToTop.h"
+
+@implementation TapToTop
+
+@end
