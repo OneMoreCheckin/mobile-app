9a2b04c34cc46f2de6b6bf90553879c64206cdb2
dmp42 <olivier@webitup.fr>
2013-01-13 00:59:48 +0100

    Using an ArrayController instead to simplify binding mechanism.

--- src/app/controllers/badges.js
+++ src/app/controllers/badges.js
@@ -1,7 +1,5 @@
 PhoneApp.pack('Omci.controllers', function() {
   'use strict';
 
-  this.Badges = Pa.Controller.extend({
-    contentBinding: 'Omci.model.user.badges.expertise',
-  });
+  this.Badges = Pa.types.ArrayController.extend();
 });
